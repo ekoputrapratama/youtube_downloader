8a241a682e2640648cfb5fc4074b25344e2bb58b
ekoputrapratama <muhammad.sayuti94@gmail.com>
2026-02-23 21:07:40 +0800

    chore: remove import to localization

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import GlobalStyles from '@mui/material/GlobalStyles';
 import App from "./App.tsx";
 import "./styles/tailwind.css";
-import "./common/i18n";
+
 
 const rootElement = document.querySelector("#root") as Element;
 if (!rootElement.innerHTML) {
