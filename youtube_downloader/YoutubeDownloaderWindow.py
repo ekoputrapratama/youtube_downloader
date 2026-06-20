c79662967a17ff78f3d39ea68aabf9db8572bd63
ekoputrapratama <muhammad.sayuti94@gmail.com>
2026-06-20 19:51:25 +0800

    chore: update

--- youtube_downloader/YoutubeDownloaderWindow.py
+++ youtube_downloader/YoutubeDownloaderWindow.py
@@ -2,9 +2,9 @@ from velra.windows.BrowserWindow import BrowserWindow
 from velra.core.Bridge import BridgeObject, Bridge
 import velra.config as config
 
+
 class YoutubeDownloaderWindow(BrowserWindow):
+
   def __init__(self, app):
     super().__init__(app=app)
     self.setWindowTitle("Youtube Downloader")
-
-
